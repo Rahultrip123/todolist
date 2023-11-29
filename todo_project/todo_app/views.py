95499d35c44e5a8394ff0a1336cbb6ee135337ed
Rahultrip123 <145183491+Rahultrip123@users.noreply.github.com>
2023-11-29 13:29:39 +0530

    Add files via upload

---
+++ todo_project/todo_app/views.py
@@ -0,0 +1,19 @@
+# todo_app/views.py
+
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
+from .permissions import IsOwner
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
